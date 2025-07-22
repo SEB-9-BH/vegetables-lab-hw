cf98bc5c56b298c3c99e407436a9124e98d1ca9e
Fatema-Alzaki <fatimaalzaki242@gmail.com>
2025-07-22 16:52:56 +0300

    commit message goes here

---
+++ homework/views/vegetables/Edit.jsx
@@ -0,0 +1,21 @@
+const React = require('react')
+const Layout = require('../layouts/layout')
+
+function Edit (props) {
+    const { name, _id, readyToEat, color } = props.vegetable
+
+    return(
+        <Layout vegetable={props.vegetable}>
+            <h1>{name} Edit Page</h1>
+            <a href='/vegetables'>Go back to Index Page</a>
+            <form action={`/vegetables/${_id}?_method=PUT`} method="POST">
+                Name: <input type="text" name="name" defaultValue={name} /><br/>
+                Color: <input type="text" name="color" defaultValue={color}/><br/>
+                Is Ready To Eat: {readyToEat? <input type="checkbox" name="readyToEat" defaultChecked />: <input type='checkbox' name="readyToEat"/>}<br/>
+                <input type="submit" value="Update vegetable" />
+            </form>
+        </Layout>
+    )
+}
+
+module.exports = Edit
---
+++ homework/views/vegetables/Index.jsx
@@ -0,0 +1,21 @@
+const React = require('react')
+const Layout = require('../layouts/layout')
+
+
+function Index (props){
+    const vegetables = props.vegetables
+    return (
+        <div>
+            <h1>Index Page</h1>
+            <ul>
+                {
+                   vegetables.map((vegetable) => {
+                    return (<li>This is the <a href={`/vegetables/${vegetable.id}`}>{vegetable.name}</a> of the color {vegetable.color}</li>)
+                   }) 
+                }
+            </ul>
+        </div>
+    )
+}
+
+module.exports = Index
