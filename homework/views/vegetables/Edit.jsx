dc98fa8ea0c90575b587cf2ed10e2f69cb28eaa1
Hawra Ayoob <hawraalkulaib.01@gmail.com>
2025-07-23 12:58:38 +0300

    Edit.jsx

--- homework/views/vegetables/Edit.jsx
+++ homework/views/vegetables/Edit.jsx
@@ -1,11 +1,12 @@
 const React = require('react')
-const Layout = require('../layouts/Layout')
+
 
 function Edit (props) {
     const { name, _id, readyToEat, color } = props.vegetable
 
     return(
-        <Layout vegetable={props.vegetable}>
+
+            <div>
             <h1>{name} Edit Page</h1>
             <a href='/vegetables'>Go back to Index Page</a>
             <form action={`/vegetables/${_id}?_method=PUT`} method="POST">
@@ -14,7 +15,8 @@ function Edit (props) {
                 Is Ready To Eat: {readyToEat? <input type="checkbox" name="readyToEat" defaultChecked />: <input type='checkbox' name="readyToEat"/>}<br/>
                 <input type="submit" value="Update vegetable" />
             </form>
-        </Layout>
+            </div>
+
     )
 }
 
